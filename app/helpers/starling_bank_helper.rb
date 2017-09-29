d2d1843b2155e5db0de0c5f60b6674a151152917
Jayshree Anandakumar <jayshree@streamoid.com>
2017-09-29 10:37:12 +0100

    Startling bacnk controller

---
+++ app/helpers/starling_bank_helper.rb
@@ -0,0 +1,2 @@
+module StarlingBankHelper
+end
---
+++ test/controllers/starling_bank_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StarlingBankControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
