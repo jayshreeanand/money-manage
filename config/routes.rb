2647887dfadb476b8fee429216852993e13f9463
Jayshree Anandakumar <jayshree@streamoid.com>
2017-09-29 11:14:12 +0100

    Set default home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
   devise_for :users
   
+  get '/' => 'pages#home'
   get '/truelayer' => 'truelayer#authenticate'
   get '/truelayer/callback' => 'truelayer#callback'
   get '/truelayer/access_token_callback' => 'truelayer#access_token_callback'
