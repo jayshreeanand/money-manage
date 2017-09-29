b9f8081fea9d4b4c399c03bcb96e42bb2c848117
Jayshree Anandakumar <jayshree@streamoid.com>
2017-09-29 14:52:01 +0100

    Transactions model

---
+++ db/migrate/20170929134916_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :transactions do |t|
+      t.belongs_to :bank_account, index: true
+      t.float :amount
+      t.float :balance
+      t.integer :kind
+      t.text :description
+      t.string :currency
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929125646) do
+ActiveRecord::Schema.define(version: 20170929134916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 20170929125646) do
     t.datetime "updated_at", null: false
     t.text     "meta_info"
     t.index ["user_id"], name: "index_bank_accounts_on_user_id", using: :btree
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "bank_account_id"
+    t.float    "amount"
+    t.float    "balance"
+    t.integer  "kind"
+    t.text     "description"
+    t.string   "currency"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["bank_account_id"], name: "index_transactions_on_bank_account_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
